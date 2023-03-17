6d26926ae61e99f2aca2672d2656262324152e15
Amiel Nava <amieln@icloud.com>
2023-03-17 12:33:50 -0700

    Added material UI, material icons and the new header

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,10 +1,25 @@
-import MainHeader from "@/components/layout/MainHeader";
-import NavigationLinks from "@/components/layout/NavigationLinks";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function MainNavigation() {
   return (
-    <MainHeader>
-      <NavigationLinks />
-    </MainHeader>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Home
+          </Typography>
+          <Button color="inherit">Location</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
