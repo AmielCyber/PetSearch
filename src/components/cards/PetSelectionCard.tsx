add009aa87d896b3deacd9bc9c644e3cb147bff1
Amiel Nava <amieln@icloud.com>
2023-03-23 16:03:52 -0700

    Update PetSelectionCard.tsx

--- src/components/cards/PetSelectionCard.tsx
+++ src/components/cards/PetSelectionCard.tsx
@@ -1,14 +1,20 @@
 import Paper from "@mui/material/Paper";
 import Link from "next/link";
-import CatIcon from "@/components/icons/CatIcon";
+
 type Props = {
-  petUrl: string;
+  petType: string;
+  location: string;
   children: React.ReactNode;
 };
+
+// May change the pathname to support dynamic routing: /search/cats?.... /search/dogs?....
+
 export default function PetSelectionCard(props: Props) {
   return (
     <Paper elevation={10} sx={{ padding: "2rem", borderRadius: "30px" }}>
-      <Link href={props.petUrl}>{props.children}</Link>
+      <Link href={{ pathname: "/search/", query: { petType: props.petType, location: props.location } }}>
+        {props.children}
+      </Link>
     </Paper>
   );
 }
