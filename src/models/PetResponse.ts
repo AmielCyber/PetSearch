6c75764717cb093be4514dab9be0dc638afb014a
Amiel Nava <amieln@icloud.com>
2023-03-28 15:04:50 -0700

    Corrected pagination link name

--- src/models/PetResponse.ts
+++ src/models/PetResponse.ts
@@ -1,15 +1,17 @@
 import Pet from "./Pet";
 
+export interface Pagination {
+  count_per_page: number;
+  total_count: number;
+  current_page: number;
+  total_pages: number;
+  _links: {
+    next: string | undefined; // Undefined if at last page
+    previous: string | undefined; // Undefined if at first page
+  };
+}
+
 export default interface PetResponse {
   pets: Pet[];
-  pagination: {
-    count_per_page: number;
-    total_count: number;
-    current_page: number;
-    total_pages: number;
-    links: {
-      next: string | undefined; // Undefined if at last page
-      previous: string | undefined; // Undefined if at first page
-    };
-  };
+  pagination: Pagination;
 }
