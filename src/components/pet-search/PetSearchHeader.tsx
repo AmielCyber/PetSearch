3f4fc5d7a72b32652007c000d8e428f78d462b39
Amiel Nava <amieln@icloud.com>
2023-03-29 14:41:06 -0700

    Updated title

--- src/components/pet-search/PetSearchHeader.tsx
+++ src/components/pet-search/PetSearchHeader.tsx
@@ -1,10 +1,15 @@
 import Typography from "@mui/material/Typography";
+
 const titleStyles = {
-  color: "#212427",
   textAlign: "center",
-  "margin-bottom": "40px",
-  marginTop: "40px",
+  "margin-bottom": "1rem",
+  marginTop: "2rem",
 };
+const zipStyles = {
+  textAlign: "center",
+  "margin-bottom": "20px",
+};
+
 type Props = {
   petType: string;
   zipCode: string;
@@ -12,8 +17,13 @@ type Props = {
 export default function PetSearchHeader(props: Props) {
   const upperCasePetType = props.petType.slice(0, 1).toUpperCase() + props.petType.slice(1);
   return (
-    <Typography sx={titleStyles} variant="h2">
-      {upperCasePetType}s close to zip code: {props.zipCode}
-    </Typography>
+    <>
+      <Typography sx={titleStyles} variant="h2">
+        {upperCasePetType}s within a 50 mile radius.
+      </Typography>
+      <Typography sx={zipStyles} variant="subtitle1">
+        Zip Code: {props.zipCode}
+      </Typography>
+    </>
   );
 }
