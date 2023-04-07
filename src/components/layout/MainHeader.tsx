aec690b8bbe354db103ad8e8de2a99ac44eb5eff
Amiel Nava <amieln@icloud.com>
2023-04-06 19:22:23 -0700

    Removed unused css and component files

--- src/components/layout/MainHeader.tsx
+++
@@ -1,8 +0,0 @@
-import styles from "@/styles/layout/MainHeader.module.css";
-
-type Props = {
-  children: React.ReactNode;
-};
-export default function MainHeader(props: Props) {
-  return <header className={styles.mainHeader}>{props.children}</header>;
-}
