b5b8e4955796d9e1d807e3cab840bd85f499dcaf
Jacqueline Vargas <vargasjacqueline96@gmail.com>
2023-04-06 21:22:04 -0700

    Moved favicon links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,26 +38,6 @@ export default function Home() {
           name="description"
           content="Search adoptable pets in your area!"
         />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
       </Head>
       <main>
         <Typography sx={titleStyles} variant="h2">
