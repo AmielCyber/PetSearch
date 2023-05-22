4e4c4f1cc1d1e069b6600cab9b6fb4a9273bb260
Amiel Nava <amieln@icloud.com>
2023-05-22 15:49:45 -0700

    Move test files in a single directory

--- pet-search-client/src/__tests__/components/layout/LocationModal.test.tsx
+++ pet-search-client/src/__tests__/components/layout/LocationModal.test.tsx
@@ -2,7 +2,7 @@
  * @vitest-environment jsdom
  */
 import {cleanup, fireEvent, render, screen} from "@testing-library/react";
-import LocationModal from "./LocationModal";
+import LocationModal from "../../../components/layout/LocationModal.tsx";
 import {afterEach, beforeEach, describe, expect} from "vitest";
 
 const onSubmit = (newZipCode: string) => console.log(newZipCode);
