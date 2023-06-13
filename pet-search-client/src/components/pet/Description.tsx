3e1c8ddba7c003768673a405d181873e3b4cfc50
Amiel Nava <amieln@icloud.com>
2023-06-13 16:49:44 -0700

    Auto top page, Back to results, and style change.
    * On next page, browser goes to the top of the page
    * Back to results in pet viewer if user came from search results

--- pet-search-client/src/components/pet/Description.tsx
+++ pet-search-client/src/components/pet/Description.tsx
@@ -23,7 +23,7 @@ export default function Description(props: Props) {
       <Typography variant="body1">{decodedDescription}</Typography>
 
       <MaterialLink href={props.url}>
-        More Info at PetFinder.
+        More at PetFinder.com.
       </MaterialLink>
     </section>
   );
