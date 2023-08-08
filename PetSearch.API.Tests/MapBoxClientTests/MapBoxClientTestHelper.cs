a34214c4fc4996138745ee86ab1ae0b840cc7dea
Amiel Nava <amieln@icloud.com>
2023-08-07 21:08:22 -0700

    Location endpoint tests

---
+++ PetSearch.API.Tests/MapBoxClientTests/MapBoxClientTestHelper.cs
@@ -0,0 +1,19 @@
+using System.Net;
+using ErrorOr;
+using PetSearch.API.Common.Errors;
+
+namespace PetSearch.API.Tests.MapBoxClientTests;
+
+public static class MapBoxClientTestHelper
+{
+    public static Error GetExpectedErrorType(HttpStatusCode responseStatusCode)
+    {
+        return responseStatusCode switch
+        {
+            HttpStatusCode.BadRequest => Errors.Location.BadRequest,
+            HttpStatusCode.Unauthorized => Errors.Token.NotAuthorized,
+            HttpStatusCode.NotFound => Errors.Location.NotFound,
+            _ => Errors.Location.ServerError,
+        };
+    }
+}
