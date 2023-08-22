8fef14ec9ce79484d673f589e76edec68983ef93
Amiel Nava <amieln@icloud.com>
2023-08-21 22:15:18 -0700

    Default starting theme from system preference

--- pet-search-client/src/App.tsx
+++ pet-search-client/src/App.tsx
@@ -1,37 +1,40 @@
-import { useState, useMemo } from "react";
+import {useState, useMemo} from "react";
 import {CssBaseline, Container, ThemeProvider, SxProps, Theme, responsiveFontSizes} from "@mui/material";
 import {SnackbarProvider} from "notistack";
 import {Outlet, ScrollRestoration} from "react-router-dom";
 // Our imports.
-import { darkModeTheme, lightModeTheme } from "./theme/materialTheme";
-import { LocationProvider } from "./hooks/LocationContext";
+import {darkModeTheme, lightModeTheme} from "./theme/materialTheme";
+import useMediaQuery from '@mui/material/useMediaQuery';
+import {LocationProvider} from "./hooks/LocationContext";
 import NavBar from "./components/layout/NavBar";
 
 const containerSx: SxProps<Theme> = {
-  paddingBottom: "2rem"
+    paddingBottom: "2rem"
 }
 
 export default function App() {
-  const [isDarkMode, setIsDarkMode] = useState(true);
+    const [isDarkMode, setIsDarkMode] = useState(
+        useMediaQuery('(prefers-color-scheme: dark)')
+    );
 
-  const handleToggleDarkMode = () => {
-    setIsDarkMode((currIsDarkMode) => !currIsDarkMode);
-  };
+    const handleToggleDarkMode = () => {
+        setIsDarkMode((currIsDarkMode) => !currIsDarkMode);
+    };
 
-  const theme =
-      useMemo(() => isDarkMode? responsiveFontSizes(darkModeTheme) : responsiveFontSizes(lightModeTheme), [isDarkMode]);
+    const theme =
+        useMemo(() => isDarkMode ? responsiveFontSizes(darkModeTheme) : responsiveFontSizes(lightModeTheme), [isDarkMode]);
 
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <SnackbarProvider autoHideDuration={3000} dense={true} maxSnack={2}/>
-      <LocationProvider>
-        <NavBar isDarkMode={isDarkMode} onToggleDarkMode={handleToggleDarkMode} />
-        <Container maxWidth="xl" sx={containerSx}>
-          <ScrollRestoration />
-          <Outlet />
-        </Container>
-      </LocationProvider>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <SnackbarProvider autoHideDuration={3000} dense={true} maxSnack={2}/>
+            <LocationProvider>
+                <NavBar isDarkMode={isDarkMode} onToggleDarkMode={handleToggleDarkMode}/>
+                <Container maxWidth="xl" sx={containerSx}>
+                    <ScrollRestoration/>
+                    <Outlet/>
+                </Container>
+            </LocationProvider>
+        </ThemeProvider>
+    );
 }
