b42f85dfb0d833e9ae456aa74b89495909a74d97
Amiel Nava <amieln@icloud.com>
2023-09-13 20:53:11 -0700

    Remove serving client app in our server

--- pet-search-client/src/components/pet/PageTitle.tsx
+++
@@ -1,8 +0,0 @@
-import { Typography } from "@mui/material";
-
-type Props = {
-  name: string;
-};
-export default function PageTitle(props: Props) {
-  return <Typography variant="h2">{props.name}</Typography>;
-}
