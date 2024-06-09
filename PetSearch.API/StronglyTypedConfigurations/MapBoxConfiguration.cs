ce0411db2f131b9922851f677f6238cf744b8736
Amiel Nava <amieln@icloud.com>
2024-06-09 11:24:16 -0700

    Moved validation to option class

--- PetSearch.API/StronglyTypedConfigurations/MapBoxConfiguration.cs
+++ PetSearch.API/StronglyTypedConfigurations/MapBoxConfiguration.cs
@@ -1,6 +1,28 @@
 namespace PetSearch.API.StronglyTypedConfigurations;
 
+/// <summary>
+/// MapBoxConfiguration for storing access token in a Singleton.
+/// </summary>
 public class MapBoxConfiguration
 {
-    public string? AccessToken { init; get; }
+    private const string AccessTokenKey = "access_token";
+
+    private readonly Dictionary<string, string> _queryParameters = new()
+    {
+        { "country", "us" },
+        { "limit", "1" },
+        { "types", "postcode" },
+        { "language", "en" },
+    };
+
+    public required string AccessToken { init; get; }
+
+    public Dictionary<string, string> QueryParameters
+    {
+        get
+        {
+            _queryParameters.TryAdd(AccessTokenKey, AccessToken);
+            return _queryParameters;
+        }
+    }
 }
